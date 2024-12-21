a62d29db156d75b859ed946f71eedda22f61e3a9
vedghimi2054 <ghimires2054@gmail.com>
2024-12-21 22:57:54 +0545

    validation change

--- src/main/java/com/company/artistmgmt/dto/UserResDto.java
+++ src/main/java/com/company/artistmgmt/dto/UserResDto.java
@@ -2,10 +2,6 @@ package com.company.artistmgmt.dto;
 
 import com.company.artistmgmt.model.general.Gender;
 import com.company.artistmgmt.model.general.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -14,19 +10,12 @@ import java.time.LocalDateTime;
 public class UserResDto {
     private int id;
     private String firstName;
-    @NotBlank(message = "Last name is required")
     private String lastName;
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
     private String email;
-    @NotBlank(message = "Phone is required")
-    @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
     private String phone;
     private LocalDateTime dob;
     private Gender gender;
-    @NotBlank(message = "Address is required")
     private String address;
-    @NotNull(message = "Role is required")
     private Role role;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
