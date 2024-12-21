91b172ee0c6a4263520df1dd8f1354dd88385b6d
vedghimi2054 <ghimires2054@gmail.com>
2024-12-21 12:21:29 +0545

    remove

--- src/main/java/com/company/artistmgmt/constant/AppConstant.java
+++ src/main/java/com/company/artistmgmt/constant/AppConstant.java
@@ -3,10 +3,6 @@ package com.company.artistmgmt.constant;
 public abstract class AppConstant {
 
     public static final long TOKEN_EXPIRATION_TIME = 60 * 60 * 1000; // 20 minutes in milliseconds
-    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 1000; // 1 hour in milliseconds
-
-    public static final String USERNAME_PREFIX = "TL";
-    public static final String DEFAULT_ERROR = "Tree Leaf default error.";
 
 
 }
